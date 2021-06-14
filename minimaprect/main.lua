ca07fcf8347774d6af83d394186ebbeda2f03c73
negbook <negbook@gmail.com>
2021-06-14 15:58:55 +0800

    Update main.lua

--- minimaprect/main.lua
+++ minimaprect/main.lua
@@ -1,25 +1,43 @@
-CreateThread(function()
-    while true do Wait(0)
-    SetScriptGfxAlign(string.byte('L'), string.byte('B'))
-    local minimapTopX, minimapTopY = GetScriptGfxPosition(-0.0045, 0.002 + (-0.188888))
+function GetMinimapAnchor() --https://forum.cfx.re/t/release-utility-minimap-anchor-script/81912
+    -- Safezone goes from 1.0 (no gap) to 0.9 (5% gap (1/20))
+    -- 0.05 * ((safezone - 0.9) * 10)
+    local safezone = GetSafeZoneSize()
+    local safezone_x = 1.0 / 20.0
+    local safezone_y = 1.0 / 20.0
+    local aspect_ratio = GetAspectRatio(0)
+    local res_x, res_y = GetActiveScreenResolution()
+    local xscale = 1.0 / res_x
+    local yscale = 1.0 / res_y
+    local Minimap = {}
+    
+    Minimap.width = xscale * (res_x / (4 * aspect_ratio))
+    Minimap.height = yscale * (res_y / 5.674) *1.33
+    SetScriptGfxAlign(string.byte('L'), string.byte('B')) --https://forum.cfx.re/t/useful-snippet-getting-the-top-left-of-the-minimap-in-screen-coordinates/712843
+    Minimap.left_x, Minimap.top_y = GetScriptGfxPosition(-0.0045+0.005, (0.002 + (-0.188888)+0.005)*1.33)
     ResetScriptGfxAlign()
-    local w, h = GetActiveScreenResolution()
-    local x,y = w * minimapTopX, h * minimapTopY
-
+    --Minimap.left_x = xscale * (res_x * (safezone_x * ((math.abs(safezone - 1.0)) * 10)))
+    Minimap.bottom_y = 1.0 - yscale * (res_y * (safezone_y * ((math.abs(safezone - 1.0)) * 10)))
+    Minimap.right_x = Minimap.left_x + Minimap.width
+    --Minimap.top_y = Minimap.bottom_y - Minimap.height
     
-    
-    DrawRect(minimapTopX+0.150/2, minimapTopY,0.150,0.002,50,250,50,200) --row
-    DrawRect(minimapTopX, minimapTopY+0.188888/2,0.002,0.188888,50,250,50,200) --col
-    
-    
-    DrawRect(minimapTopX+0.150/2, minimapTopY+0.188888,0.150,0.002,50,250,50,200) --row
-    DrawRect(minimapTopX+0.150, minimapTopY+0.188888/2,0.002,0.188888,50,250,50,200) --col
-    
-    
-    --Cross + 
-    DrawRect(minimapTopX+0.150/2, minimapTopY+0.188888/2,0.150,0.002,50,250,50,200) --row
-    DrawRect(minimapTopX+0.150/2, minimapTopY+0.188888/2,0.002,0.188888,50,250,50,200) --col
-    
-
+    Minimap.x = Minimap.left_x
+    Minimap.y = Minimap.top_y
+    Minimap.xunit = xscale
+    Minimap.yunit = yscale
+    return Minimap
+end
+function drawRct(x, y, width, height, r, g, b, a)
+    DrawRect(x + width/2, y + height/2, width, height, r, g, b, a)
+end
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        local ui = GetMinimapAnchor()
+        local thickness = 4 -- Defines how many pixels wide the border is
+        drawRct(ui.x, ui.y, ui.width, thickness * ui.yunit, 0, 0, 0, 255)
+        drawRct(ui.x, ui.y + ui.height, ui.width, -thickness * ui.yunit, 0, 0, 0, 255)
+        drawRct(ui.x, ui.y, thickness * ui.xunit, ui.height, 0, 0, 0, 255)
+        drawRct(ui.x + ui.width, ui.y, -thickness * ui.xunit, ui.height, 0, 0, 0, 255)
     end
 end)
+
